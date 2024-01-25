a5a5e642b0122d5068923493393b41bbfe8c09c4
Priyanshup891 <priyanshup891@gmail.com>
2024-01-25 17:30:14 +0530

    gamify_update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://transcendent-malasada-145041.netlify.app",
+    origin: "https://gamify-pp891.netlify.app/",
     credentials: true,
   })
 );
